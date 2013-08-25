d6f7412c34f07ca81156fba34fecbfa1ecbcd948
福田 孝 <Takashi@MacBook-Air.local>
2013-08-25 22:48:59 +0900

    Update carousel.js - prev/nextや現在地などナビ周りのオプション化 - 複数インスタンス作った際の挙動不具合修正 - translate3d非対応端末対応 - リファクタ

--- carousel/js/ui.js
+++ carousel/js/ui.js
@@ -1,22 +1,29 @@
 'use strict';
 
 // Common UI Library
+var support = {
+    translate3d: ('WebKitCSSMatrix'in window&&'m11'in new WebKitCSSMatrix())
+};
+
 (function(g, d){
 var ns = g.ui = {},
     i,
     il;
-ns.transform = function(elm, prop, duration){
-    elm.style.transform =
-    elm.style.webkitTransform =
-    elm.style.MozTransform =
-    elm.style.OTransform =
+ns.transform = function(elm, x, duration){
+    var style = elm.style,
+        prop = support.translate3d
+        ? 'translate3d(' + x +'px, 0, 0)'
+        : 'translateX(' + x +'px)';
+    style.transform =
+    style.webkitTransform =
+    style.MozTransform =
+    style.OTransform =
     prop;
-    elm.style.transitionDuration =
-    elm.style.webkitTransitionDuration =
-    elm.style.MozTransitionDuration =
-    elm.style.OTransitionDuration =
+    style.transitionDuration =
+    style.webkitTransitionDuration =
+    style.MozTransitionDuration =
+    style.OTransitionDuration =
     duration;
-    
 }
 ns.preventDefault = function(){
     event.preventDefault();
@@ -28,147 +35,156 @@ ns.preventDefault = function(){
 var ns = g.ui,
     i,
     il;
-function Carousel(opt){
-    this.carouselElm = opt.carouselElm;
-    this.liWrapElm = opt.liWrapElm;
-    this.liElms = opt.liElms;
-    this.liWidth = opt.liWidth;
-    this.liIndex = opt.liIndex || 0;
-    this.navElms = opt.navElms || 0;
-    this.prevBtnElm = opt.prevBtnElm;
-    this.nextBtnElm = opt.nextBtnElm;
-    this.loopTime = opt.loopTime || 0;
+ns.Carousel = function(){
+    this.init.apply(this, arguments);
+}
+var fn = ns.Carousel.prototype;
 
-    if(this.loopTime !== 0){
-        this.autoLoop();
+fn.init = function(element, opt){
+    var self = this;
+    self.element = element;
+    if (typeof element === 'string') {
+        self.element = document.querySelector(element);
     }
-    this.init();
+    self.liWrapElm = element.querySelector(opt.liWrapElm);
+    self.liElms = element.querySelectorAll(opt.liElms);
+    self.liWidth = opt.liWidth;
+    self.liIndex = opt.liIndex || 0;
+    self.flick = (opt.flick===undefined) ? true : opt.flick;
+    self.navElms = element.querySelectorAll(opt.navElms);
+    self.prevBtnElm = element.querySelector(opt.prevBtnElm);
+    self.nextBtnElm = element.querySelector(opt.nextBtnElm);
+    self.loopTime = opt.loopTime;
+
+    self.move(self.liIndex);
+    self.liWrapElm.style.marginLeft = -self.liWidth/2 + 'px';
+    if(self.flick){
+        self.activateFlick();
+    }
+    if(self.loopTime){
+        self.activateAutoPlay();
+    }
+    if(self.prevBtnElm || self.nextBtnElm){
+        self.activateNav();
+    }
     if(opt.debugMode == true){
-        this.debug();
+        self.debug();
     }
 }
-
-var fn = Carousel.prototype;
-
-fn.init = function(){
-    var self = this,
-        me = this.init;
-    me.liIndex = self.liIndex;
-    me.liWrapElm = self.liWrapElm;
-    me.liElms = self.liElms;
-    me.liWidth = self.liWidth;
-    me.navElms = self.navElms;
-    me.prevBtnElm = self.prevBtnElm;
-    me.nextBtnElm = self.nextBtnElm;
-
-    fn.move(self.liIndex);
-    self.liWrapElm.style.marginLeft = -self.liWidth/2 + 'px';
-    self.prevBtnElm.addEventListener('touchstart', fn.autoLoop.stop, false);
-    self.nextBtnElm.addEventListener('touchstart', fn.autoLoop.stop, false);
-
-    self.prevBtnElm.addEventListener('touchend', function(){
-        fn.prev();
-        fn.autoLoop.play();
-    }, false);
-    self.nextBtnElm.addEventListener('touchend', function(){
-        fn.next();
-        fn.autoLoop.play();
-    }, false);
-
+fn.activateFlick = function(){
+    var self = this;
     self.liWrapElm.addEventListener('touchstart', function(){
-        me.touchstartX = event.touches[0].pageX;
-        fn.swipe();
-        fn.autoLoop.stop();
+        self.touchstartX = event.touches[0].pageX;
+        self.swipe();
+        if(self.loopTime)self.autoPlay_stop();
     }, false);
     self.liWrapElm.addEventListener('touchmove', function(){
-        me.touchmoveX = event.touches[0].pageX;
+        self.touchmoveX = event.touches[0].pageX;
     }, false);
     self.liWrapElm.addEventListener('touchend', function(){
-        me.touchendX = event.changedTouches[0].pageX;
-        fn.autoLoop.play();
+        self.touchendX = event.changedTouches[0].pageX;
+        if(self.loopTime)self.autoPlay();
     }, false);
-
 }
-fn.autoLoop = function(){
-    var self = this,
-        me = self.autoLoop;
-    me.play = function(){
-        me.control = setInterval(function(){
-            if(fn.init.liIndex < fn.init.liElms.length - 1){
-                fn.next();
+fn.activateAutoPlay = function(){
+    var self = this;
+    self.autoPlay = function(){
+        self.control = setInterval(function(){
+            if(self.liIndex < self.liElms.length - 1){
+                self.next();
             }else{
-                fn.init.liIndex = 0;
-                fn.move(0);
+                self.liIndex = 0;
+                self.move(0);
             }
         }, self.loopTime);
     }
-    me.stop = function(){
-        clearInterval(me.control);
+    self.autoPlay_stop = function(){
+        clearInterval(self.control);
     }
-    me.play();
- }
+    self.autoPlay();
+}
+fn.activateNav = function(){
+    var self = this;
+    self.prevBtnElm.addEventListener('touchstart', self.autoPlay_stop, false);
+    self.nextBtnElm.addEventListener('touchstart', self.autoPlay_stop, false);
+    self.prevBtnElm.addEventListener('touchend', function(){
+        self.prev();
+        if(self.loopTime)self.autoPlay();
+    }, false);
+    self.nextBtnElm.addEventListener('touchend', function(){
+        self.next();
+        if(self.loopTime)self.autoPlay();
+    }, false);
+}
 
 fn.prev = function(){
-    if(fn.init.liIndex > 0){
-        fn.init.liIndex--;
-        fn.move(fn.init.liIndex);
+    var self = this;
+    if(self.liIndex > 0){
+        self.liIndex--;
+        self.move(self.liIndex);
     }else{
-        fn.move(fn.init.liIndex);
+        self.move(self.liIndex);
         console.log('これ以上戻れません');
     }
 }
 
 fn.next = function(){
-    if(fn.init.liIndex < fn.init.liElms.length - 1){
-        fn.init.liIndex++;
-        fn.move(fn.init.liIndex);
+    var self = this;
+    if(self.liIndex < self.liElms.length - 1){
+        self.liIndex++;
+        self.move(self.liIndex);
     }else{
-        fn.move(fn.init.liIndex);
+        self.move(self.liIndex);
         console.log('これ以上送れません');
     }
 }
 
 fn.move = function(index){
-    var navElms = fn.init.navElms,
-        prevBtnElm = fn.init.prevBtnElm,
-        nextBtnElm = fn.init.nextBtnElm;
-    for(i=0, il=navElms.length; i<il; i++){
-        if(i == index){
-            navElms[i].classList.add('current');
-        }else{
-            navElms[i].classList.remove('current');
+    var self = this,
+        navElms = self.navElms,
+        prevBtnElm = self.prevBtnElm,
+        nextBtnElm = self.nextBtnElm;
+    if(navElms){
+        for(i=0, il=navElms.length; i<il; i++){
+            (i == index)
+            ? navElms[i].classList.add('current')
+            : navElms[i].classList.remove('current');
         }
     }
-    if(fn.init.liIndex == 0){
-        prevBtnElm.classList.add('disable');
-        nextBtnElm.classList.remove('disable');
-    }else if(fn.init.liIndex == fn.init.liElms.length - 1){
-        nextBtnElm.classList.add('disable');
-        prevBtnElm.classList.remove('disable');
-    }else{
-        prevBtnElm.classList.remove('disable');
-        nextBtnElm.classList.remove('disable');
+    if(prevBtnElm && nextBtnElm){
+        if(self.liIndex == 0){
+            prevBtnElm.classList.add('disable');
+            nextBtnElm.classList.remove('disable');
+        }else if(self.liIndex == self.liElms.length - 1){
+            nextBtnElm.classList.add('disable');
+            prevBtnElm.classList.remove('disable');
+        }else{
+            prevBtnElm.classList.remove('disable');
+            nextBtnElm.classList.remove('disable');
+        }
     }
-    ui.transform(fn.init.liWrapElm, 'translate3d(' + fn.init.liWidth * -index +'px, 0, 0)', '300ms');
+    ui.transform(self.liWrapElm, self.liWidth * -index, '300ms');
 }
+
 fn.swipe = function(){
-    var elm = fn.init.liWrapElm;
+    var self = this,
+        elm = self.liWrapElm;
     elm.addEventListener('touchmove', drag, false);
     elm.addEventListener('touchend', toPrevNext, false);
     elm.addEventListener('touchend', listenerCancel, false);
     function drag(){
-        var diffX = fn.init.touchstartX - fn.init.touchmoveX,
-            x = fn.init.liIndex * fn.init.liWidth + diffX;
-        ui.transform(elm, 'translate3d(' + -x + 'px, 0, 0)', '0');
+        var diffX = self.touchstartX - self.touchmoveX,
+            x = self.liIndex * self.liWidth + diffX;
+        ui.transform(elm, -x, '0');
     }
     function toPrevNext(){
         var blankSpace = 50;
-        if(fn.init.touchstartX + blankSpace < fn.init.touchendX){
-            fn.prev();
-        }else if(fn.init.touchstartX - blankSpace > fn.init.touchendX){
-            fn.next();
+        if(self.touchstartX + blankSpace < self.touchendX){
+            self.prev();
+        }else if(self.touchstartX - blankSpace > self.touchendX){
+            self.next();
         }else{
-            fn.move(fn.init.liIndex);
+            self.move(self.liIndex);
         }
     }
     function listenerCancel(){
@@ -178,11 +194,12 @@ fn.swipe = function(){
     }
 }
 fn.debug = function(){
-    var sElm = d.createElement('p'),
+    var self = this,
+        sElm = d.createElement('p'),
         mElm = d.createElement('p'),
         eElm = d.createElement('p'),
         indexElm = d.createElement('p'),
-        bnrImgElms = this.liWrapElm.querySelectorAll('li img');
+        bnrImgElms = self.liWrapElm.querySelectorAll('li img');
 
     d.body.appendChild(sElm);
     d.body.appendChild(mElm);
@@ -192,25 +209,21 @@ fn.debug = function(){
     sElm.innerHTML = "touchstart X : " + 0;
     mElm.innerHTML = "touchmove X : " + 0;
     eElm.innerHTML = "touchend X : " + 0;
-    indexElm.innerHTML = "index : " + fn.init.liIndex;
-    this.liWrapElm.addEventListener('touchstart', function(){
-        sElm.innerHTML = "touchstart X : " + fn.init.touchstartX;
+    indexElm.innerHTML = "index : " + self.liIndex;
+    self.liWrapElm.addEventListener('touchstart', function(){
+        sElm.innerHTML = "touchstart X : " + self.touchstartX;
     }, false);
-    this.liWrapElm.addEventListener('touchmove', function(){
-        mElm.innerHTML = "touchmove X : " + fn.init.touchmoveX;
+    self.liWrapElm.addEventListener('touchmove', function(){
+        mElm.innerHTML = "touchmove X : " + self.touchmoveX;
     }, false);
-    this.liWrapElm.addEventListener('touchend', function(){
-        eElm.innerHTML = "touchend X : " + fn.init.touchendX;
+    self.liWrapElm.addEventListener('touchend', function(){
+        eElm.innerHTML = "touchend X : " + self.touchendX;
     }, false);
-    this.liWrapElm.addEventListener('webkitTransitionEnd', function(){
-        indexElm.innerHTML = "index : " + fn.init.liIndex;
+    self.liWrapElm.addEventListener('webkitTransitionEnd', function(){
+        indexElm.innerHTML = "index : " + self.liIndex;
     }, false);
     for(i=0, il=bnrImgElms.length; i<il; i++){
         bnrImgElms[i].addEventListener('touchstart', ui.preventDefault, false);
     }
 }
-ns.Carousel = Carousel;
 })(this, this.document);
-
-
-
